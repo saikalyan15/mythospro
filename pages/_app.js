950618ed0db9e143f1275708245289a72f0acf78
saikalyan15 <saikalyan15@example.com>
2025-09-08 23:57:33 +0530

    chore(ui): add favicon link in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
