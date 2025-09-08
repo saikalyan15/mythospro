d57fbe1a70e6cded6bea8ed18f7416e0afc3c412
saikalyan15 <saikalyan15@example.com>
2025-09-09 00:01:53 +0530

    chore(ui): remove header logo

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,7 @@
 import Footer from "./Footer";
-import Image from "next/image";
-
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col text-white">
-      <header className="py-4 px-6 bg-[#07101A] border-b border-[#08202D]">
-        <div className="max-w-6xl mx-auto flex items-center gap-4">
-          <div className="w-10 h-10 relative">
-            <Image src="/MythosPro-Transparent.png" alt="MythosPro" width={40} height={40} />
-          </div>
-          <div className="font-semibold">MythosPro</div>
-        </div>
-      </header>
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
